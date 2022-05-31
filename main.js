d5ea7c300b896ef5fa57d3029eede79c8d38ade2
TheWizardo <oz.sabbag@gmail.com>
2022-06-01 01:25:42 +0300

    added back button

--- main.js
+++ main.js
@@ -1,27 +1,37 @@
 (function () {
+    let baseURL = `https://pokeapi.co/api/v2/pokemon-shape`;
     document.addEventListener('DOMContentLoaded', async function () {
-        let pokeShapes = await getDataFromURL(`https://pokeapi.co/api/v2/pokemon-shape`);
-        loadDivs(pokeShapes, "results")
+        await loadDivs("", "", baseURL, "results");
     });
 })();
 
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
 async function getDataFromURL(url) {
-    let rawRes = await fetch(url)
+    let rawRes = await fetch(url);
     return rawRes.json();
 }
 
-function loadDivs(objs, enrty) {
-    for (let obj of objs[enrty]) {
+async function loadDivs(grandparentURL, parentURL, selfURL, enrty) {
+    console.log(selfURL, enrty);
+    let data = await getDataFromURL(selfURL);
+    for (let obj of data[enrty]) {
         let newDiv = document.createElement("div");
         newDiv.innerText = obj.name !== undefined ? capitalize(obj.name) : capitalize(obj.pokemon.name);
-        newDiv.setAttribute("next-info", obj.url || obj.pokemon.url)
-        newDiv.addEventListener("click", divClicked)
+        newDiv.setAttribute("grandparent-info", grandparentURL);
+        newDiv.setAttribute("parent-info", parentURL);
+        newDiv.setAttribute("self-info", selfURL);
+        newDiv.setAttribute("next-info", obj.url || obj.pokemon.url);
+        newDiv.addEventListener("click", divClicked);
         newDiv.className = `option-div`;
         document.getElementById("container-div").append(newDiv);
     }
 }
 
 function divClicked(event) {
+    document.getElementById("back-btn").disabled = false;
     (async () => {
         const progBar = document.getElementById("prog");
         progBar.value += 100 / 3;
@@ -29,8 +39,7 @@ function divClicked(event) {
 
         if (Math.floor(progBar.value) < 80) {
             document.getElementById("container-div").innerHTML = '';
-            let data = await getDataFromURL(nextURL);
-            loadDivs(data, Math.floor(progBar.value) < 40 ? "pokemon_species" : "varieties");
+            await loadDivs(event.target.getAttribute("parent-info"), event.target.getAttribute("self-info"), nextURL, Math.floor(progBar.value) < 40 ? "pokemon_species" : "varieties");
         }
         else {
             showPokemon(nextURL)
@@ -72,7 +81,7 @@ function showPokemon(url) {
             cell.innerText = capitalize(a.ability.name);
             abilityRow.appendChild(cell);
         }
-        for(let s of pokemon.stats){
+        for (let s of pokemon.stats) {
             let statProg = document.getElementById(`${s.stat.name}-stat`);
             statProg.addEventListener("mouseover", mouseHover)
             statProg.addEventListener("mouseleave", mouseExit)
@@ -83,15 +92,7 @@ function showPokemon(url) {
     })();
 }
 
-function mouseHover(event){
-    updateTooltip(event);
-    let tooltip = document.getElementById("tooltip-span");
-    tooltip.style.display = "block";
-    tooltip.innerText = event.target.value;
-    document.addEventListener("mousemove", updateTooltip);
-}
-
-function updateTooltip(event){
+function updateTooltip(event) {
     let x = event.clientX;
     let y = event.clientY;
     let tooltip = document.getElementById("tooltip-span");
@@ -99,7 +100,15 @@ function updateTooltip(event){
     tooltip.style.left = (x + 20) + 'px';
 }
 
-function mouseExit(event){
+function mouseHover(event) {
+    updateTooltip(event);
+    let tooltip = document.getElementById("tooltip-span");
+    tooltip.style.display = "block";
+    tooltip.innerText = event.target.value;
+    document.addEventListener("mousemove", updateTooltip);
+}
+
+function mouseExit(event) {
     document.removeEventListener("mousemove", updateTooltip);
     document.getElementById("tooltip-span").style.display = "none";
 }
@@ -137,9 +146,22 @@ function hUnitConvert(event) {
 }
 
 function closeModal() {
+    document.getElementById("prog").value -= 100 / 3;
     document.getElementById("pokeModal").style.display = "none";
 }
 
-function capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1)
+function goBack(btn) {
+    const progBar = document.getElementById("prog");
+    progBar.value -= 100 / 3;
+    if (progBar.value < 10) {
+        btn.disabled = true;
+    }
+
+    let firstDiv = document.getElementById("container-div").firstChild;
+    let parentURL = firstDiv.getAttribute("parent-info");
+    let grandparentURL = firstDiv.getAttribute("grandparent-info");
+    document.getElementById("container-div").innerHTML = '';
+    (async () => {
+        await loadDivs("", grandparentURL, parentURL, Math.floor(progBar.value) < 20 ? "results" : "pokemon_species");
+    })();
 }
